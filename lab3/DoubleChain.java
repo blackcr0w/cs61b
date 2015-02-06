f900e68ba79c9780844921d3f4ddd065f57ee8d0
jackzhao-mj <jackzhao.mj@gmail.com>
2015-02-06 15:40:54 -0800

    add DoubleChain

--- lab3/DoubleChain.java
+++ lab3/DoubleChain.java
@@ -4,7 +4,7 @@ public class DoubleChain {
 	private DNode head;
 	
 	public DoubleChain(double val) {
-		head = null; 
+		head = new DNode (val); 
 	}
 
 	public DNode getFront() {
@@ -14,27 +14,66 @@ public class DoubleChain {
 	/** Returns the last item in the DoubleChain. */		
 	public DNode getBack() {
 		/* your code here */
-		return null;
+		DNode p = head;
+		while (p.next != null) {
+			p = p.next;
+		}
+
+		return p;
 	}
 	
 	/** Adds D to the front of the DoubleChain. */	
 	public void insertFront(double d) {
 		/* your code here */
+		
+        if (head.next == null) {
+        	DNode newFront = new DNode (head, head.val, null);
+        	head.next = newFront;
+        	head.val = d;
+        	
+        }
+        else {
+        	DNode oldFrontItem = head.next;
+        	DNode newFrontItem = new DNode(head, head.val, oldFrontItem);
+        	head.next = newFrontItem;
+        	oldFrontItem.prev = newFrontItem;
+        	head.val = d;
+        }
+        /********size = size + 1*/
+
 	}
 	
 	/** Adds D to the back of the DoubleChain. */	
 	public void insertBack(double d) {
 		/* your code here */
+        DNode p = head;
+
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new DNode(p, d, null);
+        // p.next.prev=p;
+
 	}
 	
 	/** Removes the last item in the DoubleChain and returns it. 
 	  * This is an extra challenge problem. */
 	public DNode deleteBack() {
 		/* your code here */
-		return null;
+		DNode p = head;
+		while (p != null) {
+			p = p.next;			
+		}
+		DNode newBack = p.prev;
+		newBack.next = null;
+		p.prev = null;
+		return p;
 	}
 	
 	/** Returns a string representation of the DoubleChain. 
+	  *?????????????????????
+	  *?????????????????????
 	  * This is an extra challenge problem. */
 	public String toString() {
 		/* your code here */		
@@ -46,10 +85,13 @@ public class DoubleChain {
 		public DNode next;
 		public double val;
 		
+		/*important*/
 		private DNode(double val) {
 			this(null, val, null);
 		}
 		
+		/*important*/
+
 		private DNode(DNode prev, double val, DNode next) {
 			this.prev = prev;
 			this.val = val;
