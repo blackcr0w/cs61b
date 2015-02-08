9f3deaa14d459012b1c77db0c71b67fbd030b66c
jackzhao-mj <jackzhao.mj@gmail.com>
2015-02-08 13:50:00 -0800

    submit lab2

--- lab2/Arithmetic/ArithmeticTest.java
+++ lab2/Arithmetic/ArithmeticTest.java
@@ -1,12 +1,12 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
-
+//imports are what give you access to the JUnit methods and functionality that you'll need to run JUnit tests
 public class ArithmeticTest {
 
     /** Performs a few arbitrary tests to see if the product method is correct */
 
-    @Test 
-    public void testProduct() {
+    @Test //each method that is a test will be preceded by a @Test
+    public void testProduct() {  // test method 1
         /* assertEquals for comparison of ints takes two arguments:
         assertEquals(expected, actual).
         if it is false, then the assertion will be false, and this test will fail.
@@ -20,7 +20,7 @@ public class ArithmeticTest {
     /** Performs a few arbitrary tests to see if the sum method is correct */
 
     @Test 
-    public void testSum() {
+    public void testSum() {  // test method 2
 
         assertEquals(11, Arithmetic.sum(5, 6));
         assertEquals(-1, Arithmetic.sum(5, -6));
@@ -31,5 +31,9 @@ public class ArithmeticTest {
     /* Run the unit tests in this file. */
     public static void main(String... args) {
         jh61b.junit.textui.runClasses(ArithmeticTest.class);
+        //The following line will run all tests in SomeTest when put in the main method of your test program.
+        /* ven though testSum included many assert statements, 
+         * you only saw the first failure 
+         * (even though you know that all of the asserts would have failed!) */
     }
 }
--- lab2/Triangle/TriangleTest.java
+++ lab2/Triangle/TriangleTest.java
@@ -23,11 +23,37 @@ public class TriangleTest {
     @Test
     public void testEquilateral() {
       //TODO: FILL IN
+      Triangle t2 = new Triangle(30, 40, 50);
+      String result2 = t2.triangleType();
+      assertEquals("Scalene", result2);
+
+      Triangle t3 = new Triangle(-30, 40, 50);
+      String result3 = t3.triangleType();
+      assertEquals("At least one length is less than 0!", result3);
+
+      Triangle t4 = new Triangle(10, 10, 50);
+      String result4 = t4.triangleType();
+      assertEquals("The lengths of the triangles do not form a valid triangle!", result4);
+
     }
 
     //TODO: CREATE MORE TESTS
 
+    @Test
+    public void testTriangleType() {
+        Triangle t5 = new Triangle(30, 30, 30);
+        String result5 = t5.triangleType();
+        assertEquals("Equilateral", result5);
+    }
+
     public static void main(String[] args) {
       //TODO: RUN TESTS (Look in ArithmeticTest.java main method for help!)
+      jh61b.junit.textui.runClasses(TriangleTest.class);
+
+      //The following line will run all tests in SomeTest when put in the main method of your test program.
+        /* ven though testSum included many assert statements, 
+         * you only saw the first failure 
+         * (even though you know that all of the asserts would have failed!) */
+
     }
 }
