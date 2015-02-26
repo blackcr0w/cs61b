2c800cc5f65457b8ad7fab7b67bc73720a5767e2
jackzhao-mj <jackzhao.mj@gmail.com>
2015-02-25 20:56:44 -0800

    make a git mistake again.

---
+++ lab6/creatures/Clorus.java
@@ -0,0 +1,16 @@
+package creatures;
+import huglife.Creature;
+import huglife.Direction;
+import huglife.Action;
+import huglife.Occupant;
+import huglife.HugLifeUtils;
+import java.awt.Color;
+import java.util.Map;
+import java.util.List;
+
+/** Another implementation of a motile pacifist photosynthesizer.
+ *  @author Josh Hug
+ */
+public class Clorus extends Creature {
+	
+}
