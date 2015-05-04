1bc3efde094cf182393d0613459420dfe05ea473
jackzhao-mj <jackzhao.mj@gmail.com>
2015-05-03 17:18:35 -0700

    Made little modification

--- proj3/Trie.java
+++ proj3/Trie.java
@@ -2,7 +2,7 @@
  * Prefix-Trie. Supports linear time find() and insert(). 
  * Should support determining whether a word is a full word in the 
  * Trie or a prefix.
- * @author MJ
+ * @author MJ.
  */
 
 import java.io.*;
